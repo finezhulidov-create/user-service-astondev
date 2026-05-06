f72da28d41659b5f317f1738360fa452804d4db5
ZhulidovAl <fine.zhulidov@gmail.com>
2026-05-06 19:58:45 +0500

    feat:

--- src/main/java/com/zhulidov/user_service_astondev/UserServiceAstondevApplication.java
+++ src/main/java/com/zhulidov/user_service_astondev/UserServiceAstondevApplication.java
@@ -5,7 +5,7 @@ import com.zhulidov.user_service_astondev.config.Application;
 
 import com.zhulidov.user_service_astondev.vision.UserServiceConsoleInterface;
 
-import java.lang.reflect.InvocationTargetException;
+
 
 
 public class UserServiceAstondevApplication {
