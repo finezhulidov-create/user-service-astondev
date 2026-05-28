298703caf6e347bc3cfcb86efe038a5d2ae3c25e
ZhulidovAl <fine.zhulidov@gmail.com>
2026-05-28 15:34:31 +0500

    feat: create spring api

--- src/main/java/com/zhulidov/user_service_astondev/UserServiceAstondevApplication.java
+++ src/main/java/com/zhulidov/user_service_astondev/UserServiceAstondevApplication.java
@@ -1,15 +1,14 @@
 package com.zhulidov.user_service_astondev;
 
 
-
-
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceAstondevApplication {
 
     public static void main(String[] args)  {
-
+        SpringApplication.run(UserServiceAstondevApplication.class, args);
     }
 
 }
